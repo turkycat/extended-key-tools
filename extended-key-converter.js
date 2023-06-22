9166bc587bf4d4b72fbbfec7a5fc9b30a0a85362
Jesse Frush <jesse.frush@gmail.com>
2023-06-22 00:24:19 -0700

    add ability to convert private ext key to public version

--- extended-key-converter.js
+++ extended-key-converter.js
@@ -6,8 +6,9 @@ const ecc = require('tiny-secp256k1');
 const bip32 = BIP32Factory(ecc);
 
 program
-    .requiredOption('-k, --key <key>', 'the extended key to decode')
-    .parse(process.argv);
+  .requiredOption('-k, --key <key>', 'the extended key to decode')
+  .option('-p, --public', 'convert extended private key to public key')
+  .parse(process.argv);
 
 const options = program.opts();
 
@@ -16,10 +17,20 @@ testnetChars = [ 't', 'u', 'v', 'U', 'V' ];
 let network = bitcoin.networks.bitcoin;
 let convertTo = bitcoin.networks.testnet;
 if (testnetChars.includes(options.key[0])) {
-    network = bitcoin.networks.testnet;
-    convertTo = bitcoin.networks.bitcoin;
+  network = bitcoin.networks.testnet;
+  convertTo = bitcoin.networks.bitcoin;
 }
 const b32Key = bip32.fromBase58(options.key, network);
+
+if (options.public) {
+  if (!b32Key.privateKey) {
+    console.log('given key is already public');
+    process.exit(1);
+  }
+
+  console.log(b32Key.neutered().toBase58());
+  process.exit(0);
+}
 
 b32Key.network = convertTo;
 console.log(b32Key.toBase58());
