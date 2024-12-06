da0d6e97722cf6711d6ad6c62c7d49d9a8865227
Jesse Frush <jesse.frush@gmail.com>
2024-12-05 19:56:00 -0800

    add a generic address converter to replace the bech32 converter

---
+++ address-converter.js
@@ -0,0 +1,60 @@
+const { Option, Command } = require("commander");
+const program = new Command();
+const bitcoin = require("bitcoinjs-lib");
+const { BIP32Factory } = require("bip32");
+const ecc = require("tiny-secp256k1");
+const bip32 = BIP32Factory(ecc);
+
+program
+  .addHelpText("before", "converts a given address between networks.")
+  .requiredOption("-a, --address <address>", "the address to convert")
+  .addOption(
+    new Option("-n, --network <network>", "the network to convert to").choices([
+      "mainnet",
+      "bitcoin",
+      "testnet",
+      "regtest",
+    ])
+  )
+  .parse(process.argv);
+
+const options = program.opts();
+
+const networks = {
+  mainnet: bitcoin.networks.bitcoin,
+  bitcoin: bitcoin.networks.bitcoin,
+  testnet: bitcoin.networks.testnet,
+  regtest: bitcoin.networks.regtest,
+};
+
+const convertBech32 = (address, targetNetwork) => {
+  const decoded = bitcoin.address.fromBech32(address);
+  const { data } = decoded;
+  const newAddress = bitcoin.address.toBech32(
+    data,
+    decoded.version,
+    targetNetwork.bech32
+  );
+  return newAddress;
+};
+
+const convertBase58 = (address, targetNetwork) => {
+  const decoded = bitcoin.address.fromBase58Check(address);
+  const { hash } = decoded;
+  const newAddress = bitcoin.address.toBase58Check(
+    hash,
+    targetNetwork.pubKeyHash
+  );
+  return newAddress;
+};
+
+const targetNetwork = networks[options.network];
+
+let converted;
+if (/^bc/.test(options.address)) {
+  converted = convertBech32(options.address, targetNetwork);
+} else {
+  converted = convertBase58(options.address, targetNetwork);
+}
+
+console.log(converted);
--- extended-key-converter.js
+++ extended-key-converter.js
@@ -1,19 +1,22 @@
-const { Command } = require('commander');
+const { Command } = require("commander");
 const program = new Command();
-const bitcoin = require('bitcoinjs-lib')
-const { BIP32Factory } = require('bip32');
-const ecc = require('tiny-secp256k1');
+const bitcoin = require("bitcoinjs-lib");
+const { BIP32Factory } = require("bip32");
+const ecc = require("tiny-secp256k1");
 const bip32 = BIP32Factory(ecc);
 
 program
-  .addHelpText('before', 'converts a given extended key between networks or from private to public.')
-  .requiredOption('-k, --key <key>', 'the extended key to decode')
-  .option('-p, --public', 'convert extended private key to public key')
+  .addHelpText(
+    "before",
+    "converts a given extended key between networks or from private to public."
+  )
+  .requiredOption("-k, --key <key>", "the extended key to convert")
+  .option("-p, --public", "convert extended private key to public key")
   .parse(process.argv);
 
 const options = program.opts();
 
-testnetChars = [ 't', 'u', 'v', 'U', 'V' ];
+testnetChars = ["t", "u", "v", "U", "V"];
 
 let network = bitcoin.networks.bitcoin;
 let convertTo = bitcoin.networks.testnet;
@@ -25,7 +28,7 @@ const b32Key = bip32.fromBase58(options.key, network);
 
 if (options.public) {
   if (!b32Key.privateKey) {
-    console.log('given key is already public');
+    console.log("given key is already public");
     process.exit(1);
   }
 
